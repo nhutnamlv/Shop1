176bb3ddc2e8df61c245b1f33ff7fdd2e6ba8686
nhutnam <127466205+nhutnamlv@users.noreply.github.com>
2026-05-26 12:36:54 +0700

    Add Cart
    Add Cart

---
+++ DTO/RequestCartDTO.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Shop1.DTO
+{
+    public class RequestPaymenDTO
+    {
+        public int UserId { get; set; }
+        public int OrderId { get; internal set; }
+        public string PaymentMethod { get; internal set; }
+        public int PaymentStatus { get; internal set; }
+        public DateTime? PaidAt { get; internal set; }
+    }
+}
